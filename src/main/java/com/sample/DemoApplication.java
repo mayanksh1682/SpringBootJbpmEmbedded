283ee378abea94258f32cf5f58f3dcf591dc35c1
mayanksh <mayanksh@gmail.com>
2020-06-11 18:45:04 -0400

    cleanup

--- src/main/java/com/sample/DemoApplication.java
+++ src/main/java/com/sample/DemoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
